3ae959113e75936e364a8ff971d3a3ee262dbe20
vishwajeetaissmscoe-png <vishwajeetaissmscoe@gmail.com>
2026-05-28 18:21:30 +0530

    comparison of null and undefined

---
+++ 01operations.js
@@ -0,0 +1,13 @@
+// console.log(1+2+"3");//33
+// console.log("1"+2+3);//123
+// console.log(1+2+"2"+"3");//123
+// console.log("1"+23);//123
+// //varable which first acc to otheralso respose
+
+console.log(null>0);
+console.log(null== 0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined== 0);
+console.log(undefined>=0);
