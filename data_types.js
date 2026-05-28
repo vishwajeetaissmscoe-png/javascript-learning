eba924be2f93338df49536e0d9bc8af5de83a828
vishwajeetaissmscoe-png <vishwajeetaissmscoe@gmail.com>
2026-05-28 17:44:29 +0530

    conversions

--- data_types.js
+++ data_types.js
@@ -3,15 +3,34 @@ let state = null
 console.log(typeof null)
 
 console.log(typeof undefined);
+let score = null
+console.log(typeof score);
+let stringToNumber = Number(score)
+console.log(typeof stringToNumber);
+console.log(stringToNumber);
 
 
+// score is string then Nan
+// score is null then 1
+// vish12 then NaN 
+// boolean then as if true then 1 false when 0
 
-/* 
-number 
-bigint 
-string
-boolean 
-null stand alone value 
-null is object 
-undefined valu is not define 
-symbol */
+let isloggedin= null
+let booleanIsloggedIn = Boolean(isloggedin)
+console.log(booleanIsloggedIn);
+
+// true  = true
+// "true"= true
+//1 = true
+//null =false
+
+let someNumber
+
+// number 
+// bigint 
+// string
+// boolean 
+// null stand alone value 
+// null is object 
+// undefined valu is not define 
+// symbol 
