8646f19d30d16723e6d9fc2a7e23ceee3813cbe4
vishwajeetaissmscoe-png <vishwajeetaissmscoe@gmail.com>
2026-05-28 15:46:38 +0530

    SAVE

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 3251;
+let accountEmail = "visheahjgmai;.com" 
+let accountPassward ="96321"
+accountCity = "pune"
+let accountSatus
+console.table([accountEmail,accountId,accountPassward,accountCity])
+  /* prefer not use var because of issue in block scope and functional scope */  
---
+++ data_types.js
@@ -0,0 +1,17 @@
+
+let state = null
+console.log(typeof null)
+
+console.log(typeof undefined);
+
+
+
+/* 
+number 
+bigint 
+string
+boolean 
+null stand alone value 
+null is object 
+undefined valu is not define 
+symbol */
